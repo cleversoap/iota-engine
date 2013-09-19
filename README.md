bc778b3aa588c5b20e703b669c7ca32d527fe18f
cleversoap <clever@cleversoap.com>
2013-09-19 11:52:52 +1000

    WIP of CMake includes and libraries, C++11 and glfw3 are now being added

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 A (relatively) minimal opengl based 3D graphics engine.
 
 #Requirements
-- c++11 compatible compiler
-- glfw
+- c++11 compatible compiler (clang or gcc)
+- glfw3
 - glm
 - assimp
 - opengl 3.3+
---
+++ cmake/FindGLFW3.cmake
@@ -0,0 +1,36 @@
+# Headers
+find_path(GLFW3_DIR glfw3.h DOC "Path to GLFW3 include directory."
+    HINTS
+    $ENV{GLFW_ROOT}
+    $ENV{GLFW3_ROOT}
+    PATH_SUFFIX include
+    PATHS
+    /usr/include/GLFW
+    /usr/local/include/GLFW
+    /usr/include/GL
+    /usr/local/include/GL
+    ${GLFW_ROOT_DIR}/include/
+    )
+
+message(STATUS "Found GLFW3 Headers: ${GLFW3_DIR}")
+
+# Library Path
+find_path(GLFW3_LIB_DIR libglfw3.dylib libglfw3.a libglfw3.so glfw3.lib DOC "Path to GLFW3 libraries."
+    HINTS
+    PATH_SUFFIX lib
+    PATHS
+    ${GLFW_ROOT_DIR}/lib
+    /usr/lib
+    /usr/local/lib
+)
+
+message(STATUS "Found GLFW3 Libraries: ${GLFW3_LIB_DIR}")
+
+# Library
+find_library(GLFW3_LIB DOC "GLFW Library"
+    NAMES glfw3
+    HINTS
+    ${GLFW3_LIB_DIR}
+)
+
+message(STATUS "Found GLFW3 Library: ${GLFW3_LIB}")

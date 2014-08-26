78d39fa7e8582d43e156002dfd402856ffedcd4a
cleversoap <clever@cleversoap.com>
2014-08-26 19:19:05 +1000

    Migrated towards using the internal iota-platform.h for header simplification and iota-engine.h for external usage

---
+++ iota-engine/include/iota-platform.h
@@ -0,0 +1,52 @@
+#ifndef __IOTAPLATFORM_H__
+#define __IOTAPLATFORM_H__
+
+// std includes
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <map>
+#include <vector>
+#include <string>
+
+// glew must be included before anything GL
+#include <glew.h>
+
+// glfw3
+#include <glfw3.h>
+
+// glm
+#include <glm.hpp>
+
+// Class pre-declarations
+// Eventually this will just be the interface types so as to allow for
+// extensions.
+namespace iota
+{
+    class Context;
+    class Window;
+
+    class ISceneManager;
+    class ISceneNode;
+    class IComponent;
+    class IMeshComponent;
+}
+
+// Types
+#include "types.h"
+
+// Container and Context
+#include "Context.h"
+#include "Window.h"
+
+// Scene Manager
+#include "ISceneManager.h"
+
+// Scene Nodes
+#include "ISceneNode.h"
+
+// Components
+#include "IComponent.h"
+#include "IMeshComponent.h"
+
+#endif
